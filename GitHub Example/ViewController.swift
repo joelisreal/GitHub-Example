e0bfb8e751ef02055efc115845f01ae9eb18e33c
Joel Igberase <joeli3@109d25pf0g1f8j7.local>
2019-02-01 12:22:56 -0700

    Initial Commit

---
+++ GitHub Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Example
+//
+//  Created by Joel Igberase on 2019-02-01.
+//  Copyright © 2019 Joel Igberase. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
